deef96e31534dc94b8e4875ca63dc281a7d07a7f
brentaharris <brentaustinharris@gmail.com>
2023-04-29 13:06:34 -0400

    updated dashboard to display total number of recipes and updated user name

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { UserService } from './user/user.service'
+import { RecipeService } from './recipes/recipe.service'
+import { User } from './user/user.model'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
-  currentUser!: any
-  totalRecipes!: any
+  currentUser!: User
+  totalNumberOfRecipes!: number | undefined
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     this.currentUser = this.userService.getCurrentUser()
-
+    this.totalNumberOfRecipes = this.currentUser.recipes?.length
   }
+  
+
 }
