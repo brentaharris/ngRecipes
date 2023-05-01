3c6ba1d69635c7b09e7235127708cceeb9c94a71
brentaharris <brentaustinharris@gmail.com>
2023-05-01 13:26:39 -0400

    updated

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -21,10 +21,11 @@ const userSchema: Schema = new Schema<IUser>({
 })
 
 
-// add hashed password before saving user information
+// add hashed/salt password before saving user information
 userSchema.pre('save', async function(next) {
     if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10)
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
     }
     return next()
 })
