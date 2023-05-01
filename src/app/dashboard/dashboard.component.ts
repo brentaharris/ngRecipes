0e1db036870b91c64b6e5e09c224e33f0facee45
brentaharris <brentaustinharris@gmail.com>
2023-05-01 14:04:26 -0400

    Updated subscription for # of Recipes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,26 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { UserService } from './user/user.service'
 import { RecipeService } from './recipes/recipe.service'
 import { User } from './user/user.model'
-import { map } from 'rxjs'
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   currentUser!: User
-  totalNumberOfRecipes$!: any
+  totalNumberOfRecipes$!: number
 
-  constructor(private userService: UserService, private recipeService: RecipeService) {}
+  constructor(
+    private userService: UserService, 
+    private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     this.currentUser = this.userService.getCurrentUser()
     this.recipeService.getAllRecipesByUser().subscribe(recipes => this.totalNumberOfRecipes$ = recipes.length)
   }
-  
-  getUserData() {
+
+  ngOnDestroy(): void {
+    // this.totalNumberOfRecipes$.unsubscribe() ??
   }
-  
 
 }
