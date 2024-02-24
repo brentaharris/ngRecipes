b41c7698ee35652237ddef1f1c4eaf5527f95e04
brentaharris <brentaustinharris@gmail.com>
2024-02-24 10:46:12 -0500

    not sure how I lost this

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { UserService } from './user/user.service'
+import { RecipeService } from './recipes/recipe.service'
+import { User } from './user/user.model'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  currentUser!: User;
+  totalNumberOfRecipes!: number | undefined;
+  constructor(private userService: UserService, private recipeService: RecipeService) {}
+  
+  ngOnInit(): void {
+    this.currentUser = this.userService.getCurrentUser()
+
+    this.totalNumberOfRecipes = this.currentUser.recipes?.length
+  }
+
 
 }
